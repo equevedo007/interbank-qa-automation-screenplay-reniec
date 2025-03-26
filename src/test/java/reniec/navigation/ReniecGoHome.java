66312525b7f48e429fa5de3f805d67acab7e31e4
EDINSON ISMAEL QUEVEDO HOLGUIN <quevedo.edinson@gmail.com>
2025-03-26 18:15:28 -0500

    Automatización completada con Reporte

---
+++ src/test/java/reniec/navigation/ReniecGoHome.java
@@ -0,0 +1,9 @@
+package reniec.navigation;
+
+import net.serenitybdd.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
+
+@DefaultUrl("https://www.reniec.gob.pe/portal/peruanosExtranjero.htm")
+public class ReniecGoHome extends PageObject {
+
+}
